9ebc1a410df0eeb4ac6451e7b282f8accb5ff5a1
saggeru <ylobb99@gmail.com>
2024-01-21 15:49:05 +0900

    Userモデルのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name, :image
+    validates :dokoiku_name, uniqueness: true, format: { with: /^[0-9a-zA-Z]{,2}*$/ }
+  end
+  validates :password, format: { with: ^([a-zA-Z0-9]{8,})$ message: "Include both letters and numbers" }
+
 end
