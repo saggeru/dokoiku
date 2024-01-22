badd02924a7772daf104421ffa92e632fc2b5d96
saggeru <ylobb99@gmail.com>
2024-01-22 15:41:09 +0900

    Postモデル バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  geocoded_by :place
+  after_validation :geocode, if: :place_changed?
+
+  with_options presence: true do
+    validates :title, :text, :images, :recommend, :place
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
