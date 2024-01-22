7d71382484469363f804175421eb07bc091c6f59
saggeru <ylobb99@gmail.com>
2024-01-22 15:26:16 +0900

    Postモデル/テーブル の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_21_064552) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_22_052107) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,23 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_21_064552) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.text "text2"
+    t.text "text3"
+    t.text "recomemend", null: false
+    t.text "disappointed"
+    t.string "place", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "prefecture_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +72,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_21_064552) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
