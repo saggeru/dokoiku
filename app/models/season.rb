744f14a77fa0b7123d424adda1cb0f263b581ad3
saggeru <ylobb99@gmail.com>
2024-01-23 19:27:40 +0900

    アクティブハッシュを作成

---
+++ app/models/season.rb
@@ -0,0 +1,9 @@
+class Season < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' }, { id: 2, name: '春' }, { id: 3, name: '夏' },
+    { id: 4, name: '秋' }, { id: 5, name: '冬' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :posts
+end
