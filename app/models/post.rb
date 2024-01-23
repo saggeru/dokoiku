bbcc147de417ccb3fcd310a7783cc6022b9f1911
saggeru <ylobb99@gmail.com>
2024-01-23 19:27:47 +0900

    アクティブハッシュ疑似モデルとの関連付け/バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :season
+  belongs_to :point
 
   geocoded_by :place
   after_validation :geocode, if: :place_changed?
 
   with_options presence: true do
-    validates :title, :text, :images, :recommend, :place
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :title, :text, :recommend, :place
+    validates :prefecture_id, :season_id, :point_id, numericality: { other_than: 1, message: "項目を選択してください" }
+    validates :images, length: { minimum: 1, maximum: 3, message: "は1枚以上3枚以下にしてください" }
   end
+
 end
