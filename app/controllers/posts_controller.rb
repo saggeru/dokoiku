a6ca3d990ca2e9a069933d2aedcbc43adbb71b7e
saggeru <ylobb99@gmail.com>
2024-01-24 14:12:07 +0900

    Createアクションの定義/ストロングパラメーターの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :recommend, :place, :prefecture_id, :season_id, :point_id, images: []).merge(user_id: current_user.id)
+  end
 end
