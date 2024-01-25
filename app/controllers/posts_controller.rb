1330dfcd63e25f00216a0024b02a1bb782c96708
saggeru <ylobb99@gmail.com>
2024-01-25 14:46:31 +0900

    投稿 最新5件の取得に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @posts = Post.includes(:user).order("created_at DESC")
+    @posts = Post.includes(:user).order("created_at DESC").limit(5)
   end
 
   def new
