be8291bb3fdc62860d20b78cd9643b88ef85d2a8
saggeru <ylobb99@gmail.com>
2024-02-01 17:00:46 +0900

    都道府県個別ページの表示順を変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
     @prefecture = Prefecture.find(params[:id])
-    @posts = Post.where(prefecture_id: @prefecture.id)
+    @posts = Post.where(prefecture_id: @prefecture.id).order("created_at DESC")
   end
 end
