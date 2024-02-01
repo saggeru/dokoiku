dd423e67196f299601a135febc49447ad8f75dfa
saggeru <ylobb99@gmail.com>
2024-02-01 17:34:48 +0900

    都道府県個別ページN+1問題を解決

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
-  def index
+  def show
     @prefecture = Prefecture.find(params[:id])
     @posts = Post.where(prefecture_id: @prefecture.id).order("created_at DESC")
   end
