1b94629273cd6e7ed77377aba527736c95cd66c3
saggeru <ylobb99@gmail.com>
2024-02-01 22:44:16 +0900

    class_nameを使用したUserモデルのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :posts
   has_one_attached :image
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   with_options presence: true do
     validates :name, :image
     validates :dokoiku_name, uniqueness: true, format: { with: /\A[0-9a-zA-Z]{2,}\z/ }
