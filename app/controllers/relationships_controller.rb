062b1499222ee5c0678ec6b469a99fadb857c19f
saggeru <ylobb99@gmail.com>
2024-02-02 00:11:04 +0900

    リダイレクト先を修正(ユーザーページに留まる)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
-    redirect_to root_path
+    redirect_to user_path(params[:user_id])
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
+    redirect_to user_path(params[:user_id])
   end
 end
