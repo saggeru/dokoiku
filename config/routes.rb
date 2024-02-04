09edd3c5345ec3c048560e88e33dfdd4f03a8a9a
saggeru <ylobb99@gmail.com>
2024-02-04 19:04:18 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts
   resources :searches, only: [:show]
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:show] do
     resources :relationships, only: [:create, :destroy]
   end
