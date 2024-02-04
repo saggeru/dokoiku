468e55a2971aa4debd61d742fddb07dfc15ef21f
saggeru <ylobb99@gmail.com>
2024-02-04 19:55:53 +0900

    リダイレクト先の変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
-    redirect_to root_path
+    redirect_to post_path(params[:post_id])
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    redirect_to post_path(params[:post_id])
   end
 end
